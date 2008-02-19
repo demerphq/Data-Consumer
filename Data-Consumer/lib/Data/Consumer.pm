09f98a2ef7c5245cfbf6e3efd5f8ce54b165d4d6
demerphq@gemini <demerphq@gemini>
2008-02-19 15:33:33 +0100

    added many files -- vc is cool!

---
+++ Data-Consumer/lib/Data/Consumer.pm
@@ -0,0 +1,107 @@
+package Data::Consumer;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Data::Consumer - The great new Data::Consumer!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Data::Consumer;
+
+    my $foo = Data::Consumer->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Yves Orton, C<< <YVES at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-data-consumer at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Data-Consumer>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Data::Consumer
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Data-Consumer>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Data-Consumer>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Data-Consumer>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Data-Consumer>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Yves Orton, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Data::Consumer
